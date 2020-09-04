cb119ff01d1e20d1c1b1fcdbd4db850409184a1f
Bruno Muniz <brunomunizdev@gmail.com>
2020-09-03 23:13:47 -0300

    Integration react-native-paper and create SearchbarProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,34 @@ import React from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { ApolloProvider } from '@apollo/react-hooks';
+import {
+  Provider as PaperProvider,
+  DefaultTheme as PaperDefaultTheme,
+} from 'react-native-paper';
 
 import apolloClient from './graphql/apolloClient';
+import { SearchbarProvider } from './contexts';
 
-const App: React.FC = () => <ApolloProvider client={apolloClient} />;
+const DefaultTheme = {
+  ...PaperDefaultTheme,
+  colors: {
+    ...PaperDefaultTheme.colors,
+    primary: '#E53935',
+    accent: '#FFF',
+    background: '#FFF',
+  },
+};
+
+const App: React.FC = () => (
+  <>
+    <ApolloProvider client={apolloClient}>
+      <PaperProvider theme={DefaultTheme}>
+        <NavigationContainer theme={DefaultTheme}>
+          <SearchbarProvider>{/* TODO: Rotasss */}</SearchbarProvider>
+        </NavigationContainer>
+      </PaperProvider>
+    </ApolloProvider>
+  </>
+);
 
 export default App;
---
+++ src/contexts/Searchbar.tsx
@@ -0,0 +1,28 @@
+import React, { createContext, useState } from 'react';
+
+export const SearchbarContext = createContext({});
+
+function SearchbarProvider({ children }: any) {
+  const [searchbarBoolean, setSearchbarBoolean] = useState(false);
+  const [searchbarQuery, setSearchbarQuery] = useState('');
+
+  const toggleSearchbar = () => {
+    setSearchbarBoolean(!searchbarBoolean);
+    setSearchbarQuery('');
+  };
+
+  return (
+    <SearchbarContext.Provider
+      value={{
+        searchbarBoolean,
+        toggleSearchbar,
+        searchbarQuery,
+        setSearchbarQuery,
+      }}
+    >
+      {children}
+    </SearchbarContext.Provider>
+  );
+}
+
+export default SearchbarProvider;
